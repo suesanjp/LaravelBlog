0769ade54d5feffefefd573a0c8cce52d9cbb557
Tomoaki Suzuki <sue@Tomoakis-MacBook-Pro.local>
2023-06-07 11:56:50 +0900

    add seeder

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('posts')->insert([
+            'title' => 'My first post',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        ]);
+    }
+}
