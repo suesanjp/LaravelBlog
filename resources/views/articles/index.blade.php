c486b69bd88d60d9c515fcfbac4ec4c10fcad446
Tomoaki Suzuki <sue@Tomoakis-MacBook-Pro.local>
2023-06-09 00:12:50 +0900

    installed daisyUI css components

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,11 +1,10 @@
-<x-app-layout>
-<x-slot name="header">
+<x-article-layout>
     @if (Route::has('login'))
         <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
             @auth
                 <a href="{{ url('/dashboard') }}"
                     class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">マイページ</a>
-    <p><a href="{{ route('articles.create') }}">記事を書く</a></p>
+                <p><a href="{{ route('articles.create') }}">記事を書く</a></p>
             @else
                 <a href="{{ route('login') }}"
                     class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">ログイン</a>
@@ -26,12 +25,13 @@
             <div class="article-info">{{ $article->created_at }}</div>
         </article>
         <div class="article-control">
-            <a href="{{ route('articles.edit', $article) }}">編集</a>
-            <form onsubmit="return confirm('本当に削除しますか？')" action="{{ route('articles.destroy', $article) }}" method="post">
+            <a class=" href="{{ route('articles.edit', $article) }}">編集</a>
+            <form onsubmit="return confirm('本当に削除しますか？')" action="{{ route('articles.destroy', $article) }}"
+                method="post">
                 @csrf
                 @method('delete')
                 <button type="submit">削除</button>
             </form>
         </div>
     @endforeach
-</x-app-layout>
+</x-article-layout>
--- resources/views/layouts/article.blade.php
+++ resources/views/layouts/article.blade.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html data-theme="cupcake" lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="/main.css">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
     <header>
         <div class="site-title">ミニブログ</div>
     </header>
     <main class="container">
-
-        @yield('content')
-
+        {{ $slot }}
     </main>
     <footer>
         &copy; Mini Blog
     </footer>
 </body>
+
 </html>
