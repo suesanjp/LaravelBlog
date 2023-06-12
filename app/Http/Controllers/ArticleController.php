4df621ece11a27058bc1afd3bcf2568c44ab7b9e
Tomoaki Suzuki <sue@Tomoakis-MacBook-Pro.local>
2023-06-13 05:25:56 +0900

    reverse index article direction

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -12,7 +12,7 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::all() -> reverse();
         $data = ['articles' => $articles];
         return view('articles.index', $data);
     }
